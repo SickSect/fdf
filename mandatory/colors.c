16fd5ff8e7cb457b8874af203385ccdc14c2258c
SickSect <sleepyboy906sb@gmail.com>
2022-03-04 20:35:20 +0300

    wat

--- mandatory/colors.c
+++ mandatory/colors.c
@@ -4,15 +4,15 @@
 void	change_color(t_data *data)
 {
 	if (data->way == 1)
-	{
 		data->color = create_trgb(255, 100, 255, 100);
-	}
 	else if (data->way == 2)
 		data->color = create_trgb(255, 255, 255, 255);
 	else if (data->way == 3)
-	{
 		data->color = create_trgb(255, 100, 255, 100);
-	}
 	else if (data->way == 4)
 		data->color = create_trgb(255, 255, 0, 0);
+	else if (data->way == 5)
+		data->color = create_trgb(255, 100, 100, 255);
+	else if (data->way == 6)
+		data->color = create_trgb(255, 0, 0, 255);
 }
